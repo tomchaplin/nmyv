251f6c80b5e669562b8223fc373dedc352286563
Thomas Chaplin <tomrchaplin@gmail.com>
2020-05-07 10:56:22 +0100

    Initial commit

---
+++ background.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let appName = 'com.tom.nmyv'; 
+
+function parseURL(url) {
+    var parser = document.createElement('a'),
+        searchObject = {},
+        queries, split, i;
+    // Let the browser do the work
+    parser.href = url;
+    // Convert query string to object
+    queries = parser.search.replace(/^\?/, '').split('&');
+    for( i = 0; i < queries.length; i++ ) {
+        split = queries[i].split('=');
+        searchObject[split[0]] = split[1];
+    }
+    return {
+        protocol: parser.protocol,
+        host: parser.host,
+        hostname: parser.hostname,
+        port: parser.port,
+        pathname: parser.pathname,
+        search: parser.search,
+        searchObject: searchObject,
+        hash: parser.hash
+    };
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+	chrome.browserAction.onClicked.addListener(function(tab) {
+		chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+			let url = tabs[0].url;
+
+			let parsed = parseURL(url);
+			if(parsed.searchObject.v){
+				let vid = parsed.searchObject.v;
+				console.log(vid);
+				chrome.runtime.sendNativeMessage(appName, {text: vid})
+			}
+		});
+	});
+});
---
+++ nmyv.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import nativemessaging
+import os
+import re
+import time
+
+# We check if "yvmux" is open
+# If not then we open it up with a youtube-viewer
+# The main loop is as follows
+# 1. Wait for a message
+# 2. Close a potentially open video with "q<Backspace>"
+# 3. Open the video
+
+#print("nmyv host started")
+# Check for yvmux and open if needed
+stream = os.popen('tmux list-sessions | grep yvmux | wc -l')
+numStreams = stream.read()
+numStreams = numStreams.strip('\n')
+numStreams = int(numStreams)
+if numStreams == 0:
+    os.system('tmux new -d -s yvmux')
+    os.system('tmux send-keys -t yvmux youtube-viewer Enter')
+
+# Main loop
+myRegex = re.compile('[a-zA-Z0-9_-]{11}');
+while True:
+    # Get the URL from the extension
+    message = nativemessaging.get_message()
+    # Close any currently running videos
+    os.system('tmux send-keys -t yvmux q BSpace')
+    time.sleep(1)
+    # Check that we only got a valid youtube ID
+    vidMatch = myRegex.match(message["text"]);
+    if vidMatch:
+        vid = vidMatch.string
+        # Open the new video
+        os.system('tmux send-keys -t yvmux :v='+vid+' Enter')
